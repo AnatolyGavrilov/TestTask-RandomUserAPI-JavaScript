5598b5a531614c5e3ed0859f13a5e89d175904c2
Anatoly <sharamradov@gmail.com>
2023-07-17 12:48:29 +0300

    Reritten parseJSON to xmlHttpRequest with Promise and added basic rendering

--- js/main.js
+++ js/main.js
@@ -1,26 +1,43 @@
-let getJSON = (url) => {
+let container = document.querySelector('.container');
 
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'json';
+let users = [];
 
-    xhr.onload = () => {
-        
+let drowElements = (users) => {
+  users.map((elem)=> { 
+    let element = document.createElement("div");
+    let elementGender = document.createElement("p");
+    // let elementName = document.createElement("div");
+    let elemGender = document.createTextNode(elem.gender);
+    element.appendChild(elemGender);
+    element.className="test2";
+    container.appendChild(element);
+  })
+}
+
+let xmlHttpRequest = function(url) {
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.responseType = 'json';
+        xhr.onload = () => {
         let status = xhr.status;
-
         if (status == 200) {
-            let users = [];
             xhr.response.results.map((result)=>{
                 users.push(result);
             });
-            console.log(users);
-        } else {
-            console.error(status);
-        }
-    };
-
-    xhr.send();
-};
-
-getJSON('https://randomuser.me/api/?results=25');
-
+            resolve(users);
+            } else {
+                reject(status);
+                console.error(status);
+            }
+        };
+      xhr.send();
+    });
+  };
+  
+  xmlHttpRequest('https://randomuser.me/api/?results=25')
+    .then((users) => {
+        drowElements(users);
+    }).catch(function(err){
+    console.error(err);
+  });
